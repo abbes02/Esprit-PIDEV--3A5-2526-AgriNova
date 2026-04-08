3bd39e771aa42d548821c75f5af8a50692fe9f73
Mehdi <MEHDIABBES02@GMAIL.com>
2026-04-08 17:37:31 +0100

    Map formation entities to existing schema

--- src/Entity/Evenement.php
+++ src/Entity/Evenement.php
@@ -8,70 +8,146 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: EvenementRepository::class)]
+#[ORM\Table(name: 'evenement')]
 class Evenement
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(name: 'IdEvenement')]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'evenements')]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Assert\NotNull(message: 'A formation must be selected.')]
+    #[ORM\JoinColumn(name: 'IdFormation', referencedColumnName: 'IdFormation', nullable: false)]
+    #[Assert\NotNull(message: 'Une formation doit etre selectionnee.')]
     private ?Formation $formation = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(name: 'DateDebut', type: Types::DATETIME_MUTABLE, nullable: true)]
     #[Assert\NotNull]
     private ?\DateTimeInterface $dateDebut = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[ORM\Column(name: 'DateFin', type: Types::DATETIME_MUTABLE, nullable: true)]
     #[Assert\NotNull]
-    #[Assert\GreaterThan(propertyPath: 'dateDebut', message: 'End date must be after start date.')]
+    #[Assert\GreaterThan(propertyPath: 'dateDebut', message: 'La date de fin doit etre apres la date de debut.')]
     private ?\DateTimeInterface $dateFin = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(name: 'Lieu', length: 200, nullable: true)]
     #[Assert\NotBlank]
     private ?string $lieu = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(name: 'Type', length: 50, nullable: true)]
     #[Assert\NotBlank]
     private ?string $type = null;
 
-    #[ORM\Column]
-    #[Assert\Positive(message: 'Capacity must be greater than 0.')]
+    #[ORM\Column(name: 'CapaciteMax', nullable: true)]
+    #[Assert\Positive(message: 'La capacite doit etre superieure a 0.')]
     private ?int $capaciteMax = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: 'NombreInscrits', options: ['default' => 0])]
     #[Assert\PositiveOrZero]
     private int $nombreInscrits = 0;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(name: 'Statut', length: 50, nullable: true)]
     #[Assert\NotBlank]
     private ?string $statut = null;
 
-    public function getId(): ?int { return $this->id; }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    public function getFormation(): ?Formation { return $this->formation; }
-    public function setFormation(?Formation $formation): static { $this->formation = $formation; return $this; }
+    public function getFormation(): ?Formation
+    {
+        return $this->formation;
+    }
 
-    public function getDateDebut(): ?\DateTimeInterface { return $this->dateDebut; }
-    public function setDateDebut(\DateTimeInterface $dateDebut): static { $this->dateDebut = $dateDebut; return $this; }
+    public function setFormation(?Formation $formation): static
+    {
+        $this->formation = $formation;
 
-    public function getDateFin(): ?\DateTimeInterface { return $this->dateFin; }
-    public function setDateFin(\DateTimeInterface $dateFin): static { $this->dateFin = $dateFin; return $this; }
+        return $this;
+    }
 
-    public function getLieu(): ?string { return $this->lieu; }
-    public function setLieu(string $lieu): static { $this->lieu = $lieu; return $this; }
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
 
-    public function getType(): ?string { return $this->type; }
-    public function setType(string $type): static { $this->type = $type; return $this; }
+    public function setDateDebut(?\DateTimeInterface $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
 
-    public function getCapaciteMax(): ?int { return $this->capaciteMax; }
-    public function setCapaciteMax(int $capaciteMax): static { $this->capaciteMax = $capaciteMax; return $this; }
+        return $this;
+    }
 
-    public function getNombreInscrits(): int { return $this->nombreInscrits; }
-    public function setNombreInscrits(int $nombreInscrits): static { $this->nombreInscrits = $nombreInscrits; return $this; }
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
 
-    public function getStatut(): ?string { return $this->statut; }
-    public function setStatut(string $statut): static { $this->statut = $statut; return $this; }
+    public function setDateFin(?\DateTimeInterface $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?string $lieu): static
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCapaciteMax(): ?int
+    {
+        return $this->capaciteMax;
+    }
+
+    public function setCapaciteMax(?int $capaciteMax): static
+    {
+        $this->capaciteMax = $capaciteMax;
+
+        return $this;
+    }
+
+    public function getNombreInscrits(): int
+    {
+        return $this->nombreInscrits;
+    }
+
+    public function setNombreInscrits(int $nombreInscrits): static
+    {
+        $this->nombreInscrits = $nombreInscrits;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
 }
--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -10,45 +10,46 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FormationRepository::class)]
+#[ORM\Table(name: 'formation')]
 class Formation
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(name: 'IdFormation')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(name: 'Titre', length: 150)]
     #[Assert\NotBlank]
     private ?string $titre = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(name: 'Description', type: Types::TEXT, nullable: true)]
     #[Assert\NotBlank]
     private ?string $description = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(name: 'Domaine', length: 100, nullable: true)]
     #[Assert\NotBlank]
     private ?string $domaine = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(name: 'Niveau', length: 50, nullable: true)]
     #[Assert\NotBlank]
     private ?string $niveau = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: 'DureeHeures', nullable: true)]
     #[Assert\Positive]
     private ?int $dureeHeures = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: 'Prix', nullable: true)]
     #[Assert\PositiveOrZero]
     private ?float $prix = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[ORM\Column(name: 'DateCreation', type: Types::DATE_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $dateCreation = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(name: 'Statut', length: 50, nullable: true)]
     #[Assert\NotBlank]
     private ?string $statut = null;
 
-    #[ORM\OneToMany(mappedBy: 'formation', targetEntity: Evenement::class, cascade: ['persist'])]
+    #[ORM\OneToMany(mappedBy: 'formation', targetEntity: Evenement::class)]
     private Collection $evenements;
 
     public function __construct()
@@ -57,34 +58,114 @@ class Formation
         $this->dateCreation = new \DateTime();
     }
 
-    public function getId(): ?int { return $this->id; }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    public function getTitre(): ?string { return $this->titre; }
-    public function setTitre(string $titre): static { $this->titre = $titre; return $this; }
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
 
-    public function getDescription(): ?string { return $this->description; }
-    public function setDescription(string $description): static { $this->description = $description; return $this; }
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
 
-    public function getDomaine(): ?string { return $this->domaine; }
-    public function setDomaine(string $domaine): static { $this->domaine = $domaine; return $this; }
+        return $this;
+    }
 
-    public function getNiveau(): ?string { return $this->niveau; }
-    public function setNiveau(string $niveau): static { $this->niveau = $niveau; return $this; }
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
 
-    public function getDureeHeures(): ?int { return $this->dureeHeures; }
-    public function setDureeHeures(int $dureeHeures): static { $this->dureeHeures = $dureeHeures; return $this; }
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
 
-    public function getPrix(): ?float { return $this->prix; }
-    public function setPrix(float $prix): static { $this->prix = $prix; return $this; }
+        return $this;
+    }
 
-    public function getDateCreation(): ?\DateTimeInterface { return $this->dateCreation; }
-    public function setDateCreation(\DateTimeInterface $dateCreation): static { $this->dateCreation = $dateCreation; return $this; }
+    public function getDomaine(): ?string
+    {
+        return $this->domaine;
+    }
 
-    public function getStatut(): ?string { return $this->statut; }
-    public function setStatut(string $statut): static { $this->statut = $statut; return $this; }
+    public function setDomaine(?string $domaine): static
+    {
+        $this->domaine = $domaine;
 
-    /** @return Collection<int, Evenement> */
-    public function getEvenements(): Collection { return $this->evenements; }
+        return $this;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(?string $niveau): static
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    public function getDureeHeures(): ?int
+    {
+        return $this->dureeHeures;
+    }
+
+    public function setDureeHeures(?int $dureeHeures): static
+    {
+        $this->dureeHeures = $dureeHeures;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $dateCreation): static
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
 
     public function addEvenement(Evenement $evenement): static
     {
@@ -92,16 +173,16 @@ class Formation
             $this->evenements->add($evenement);
             $evenement->setFormation($this);
         }
+
         return $this;
     }
 
     public function removeEvenement(Evenement $evenement): static
     {
-        if ($this->evenements->removeElement($evenement)) {
-            if ($evenement->getFormation() === $this) {
-                $evenement->setFormation(null);
-            }
+        if ($this->evenements->removeElement($evenement) && $evenement->getFormation() === $this) {
+            $evenement->setFormation(null);
         }
+
         return $this;
     }
 
